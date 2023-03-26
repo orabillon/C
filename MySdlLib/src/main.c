527d21ceadb7946abca668dbcd90187b6b920853
orabillon <115501074+orabillon@users.noreply.github.com>
2023-03-26 10:55:28 +0200

    Traitement image
    Installation SDL_Image
    Ajout code pour chargement et affichage image

--- MySdlLib/src/main.c
+++ MySdlLib/src/main.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL.h>
+#include <SDL_image.h>
+
+#define SDL_ASSERT_LEVEL 2
+
+SDL_Texture *LoadTexture(SDL_Renderer *renderer, char *path){
+
+    SDL_Texture *texture = NULL;
+
+    texture = IMG_LoadTexture(renderer, path);
+    if(texture == NULL){
+        printf("Impossible de creer la texture a partir de %s erreur : %s\n", path, IMG_GetError());
+        return NULL;
+    }
+
+    return texture;
+}
 
 int main(int argc, char *argv[]){
+    /* Permet l'affichage des printf sans tampons*/
+    setvbuf(stdout, NULL,_IONBF, 0);
+
     /*
         variable
     */
@@ -12,6 +31,7 @@ int main(int argc, char *argv[]){
 
     Uint32 uFlagsWindos = SDL_WINDOW_SHOWN;
     Uint32 uFlagsRenderer = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    Uint32 uFlagsSdlImage = IMG_INIT_JPG | IMG_INIT_PNG;
 
     /*
         initialisation SDL
@@ -47,6 +67,24 @@ int main(int argc, char *argv[]){
         printf("Impossible de creer le renderer : %s\n", SDL_GetError());
         return -1;
     }
+    else
+    {
+        /* Initialisation SDL IMAGE*/
+        if(!(IMG_Init(uFlagsSdlImage) & uFlagsSdlImage)){
+            printf("Impossible d'initialiser la librairie SDL_IMAGE : %s\n", IMG_GetError());
+        return -1;
+        } 
+    }
+
+   printf("Start");
+
+   /* Variable Jeu*/
+    SDL_Texture *texPlanet = LoadTexture(renderer, "assets/images/planet.png");
+
+    int texPlanetWidth = 0;
+    int textPlanetHeight = 0;
+    SDL_QueryTexture(texPlanet, NULL, NULL, &texPlanetWidth, &textPlanetHeight);
+    SDL_Rect recDestination = {0, 0, texPlanetWidth, textPlanetHeight};
 
     /*
         Game Loop
@@ -64,17 +102,23 @@ int main(int argc, char *argv[]){
         /* Efface l'écran */
         SDL_RenderClear(renderer);
 
+        /* Modification de l'écran */
+        SDL_RenderCopy(renderer, texPlanet, NULL, &recDestination);
+
         /* Affiche l'écran*/
         SDL_RenderPresent(renderer);
    }
 
+    printf("Fin Programme");
    /*
         Nettoyage des pointeur
    */
+   SDL_DestroyTexture(texPlanet);
    SDL_DestroyRenderer(renderer);
    SDL_DestroyWindow(window);
 
    /* Fermeture SDL*/
+   IMG_Quit();
    SDL_Quit();
 
    return 0;

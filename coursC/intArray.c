1ea93658b129c7fa8e251052d3ba10dddecb33e2
orabillon <115501074+orabillon@users.noreply.github.com>
2023-04-19 20:05:37 +0200

    Ajout fonction manipulation array

--- coursC/intArray.c
+++ coursC/intArray.c
@@ -65,6 +65,38 @@ intarray intArray_create(int len)
     return array;
 }
 
+void intArray_destroy(intarray array)
+{
+    free(array.data);
+}
+
+int intArray_get(intarray array, int index)
+{
+    if ((index < 0) || (index >= array.len))
+    {
+        printf("intArray_get : L'index %d est invalide.\n les valeurs valide sont entre %d et %d.\n", index, 0, array.len - 1);
+        return -1;
+    }
+
+    return array.data[index];
+}
+
+void intArray_set(intarray array, int index, int value)
+{
+    if ((index < 0) || (index >= array.len))
+    {
+        printf("intArray_set : L'index %d est invalide.\n les valeurs valide sont entre %d et %d.\n", index, 0, array.len - 1);
+        return;
+    }
+
+    array.data[index] = value;
+}
+
+int intArray_length(intarray array)
+{
+    return array.len;
+}
+
 // --------------------------------------------------------------------- TEST MDULE --------------------------------------------------------------------------------
 
 int main()
@@ -77,13 +109,12 @@ int main()
         array.data[i] = 6 * i + 4;
     }
 
-    array.data[1] = -20;
-    array.data[4] = 20;
-    array.data[6] = -20;
+    intArray_set(array, 5, 13);
 
-    intArray_positive_values(array);
+    intArray_debug(array);
+    printf("\n");
 
-    free(array.data);
+    intArray_destroy(array);
 
     return EXIT_SUCCESS;
 }
--- coursC/intArray.h
+++ coursC/intArray.h
@@ -12,11 +12,68 @@ typedef struct intarray
 
 } intarray;
 
+/**
+ * creation d'un nouveau tableau d'entier (intarray) initialise a 0 pour chaque case
+ * @param len taille du tableau
+ * @return intarray retourne le nouveau tableau
+ */
 intarray intArray_create(int len);
 
+/**
+ * Permet d'afficher le contenu d'un tableau d'intarray
+ * @param array tableau a afficher
+ */
 void intArray_debug(intarray array);
+
+/**
+ * Permet d'afficher les valeur positive d'un tableau d'intarray
+ * @param array tableau a afficher
+ */
 void intArray_positive_values(intarray array);
+
+/**
+ * Permet de rechercher une valeur dans le contenu d'un tableau d'intarray
+ * @param array tableau pour la recherche
+ * @param n nombre a rechercher
+ * @return retourn true si l'element est trouver sinon false.
+ */
 bool intArray_search(intarray array, int n);
+
+/**
+ * Compte le nombre d'occurrence d'une valeur contenu dans le tableau d'intarray
+ * @param array tableau pour la recherche
+ * @param n nombre a rechercher
+ * @return le nombre d'occurrence de la valeur demandé
+ */
 int intArray_nb_occurences(intarray array, int n);
+
+/**
+ * Désaloue un tableau
+ * @param array tableau a libéré
+ */
+void intArray_destroy(intarray array);
+
+/**
+ * Retourne la valeur contenu dans le tableau d'intarray a la case index
+ * @param array tableau pour la recherche
+ * @param index numero de la case dont on souhaite avoir le contenu
+ * @return le nombre contenu dans la case numero 'index'
+ */
+int intArray_get(intarray array, int index);
+
+/**
+ * Place une valeur dans le tableau d'intarray a la case index
+ * @param array tableau pour l'insertion
+ * @param index numero de la case dont on souhaite modifier le contenu
+ * @param value valeur a inserer dans le tableau a l'index indiquer
+ */
+void intArray_set(intarray array, int index, int value);
+
+/**
+ * Retourne la longueur d'un tableau d'intarray
+ * @param array tableau pour le calcul de la taille
+ * @return la taille du tableau
+ */
+int intArray_length(intarray array);
 
 #endif

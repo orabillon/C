00354544a89bb8a5e6d7c859ab50363345b12f95
orabillon <115501074+orabillon@users.noreply.github.com>
2023-07-16 12:21:24 +0200

    Création fichier build

---
+++ LibSDL/build.sh
@@ -0,0 +1,20 @@
+#/usr/bin/env bash
+
+rm ./bin/MonProg
+rm ./bin/assets/images/*.*
+rm ./bin/assets/fonts/*.*
+rm ./bin/assets/sons/*.*
+
+cp ./src/assets/images/*.* ./bin/assets/images/
+cp ./src/assets/fonts/*.* ./bin/assets/fonts/
+cp ./src/assets/sons/*.* ./bin/assets/sons/
+
+gcc -Wall -Werror -pedantic src/main.c src/orn/orn.c src/orn/orn_graphics.c src/orn/orn_font.c src/orn/orn_keyboard.c src/orn/orn_sound.c src/orn/orn_animation.c src/game.c -o bin/MonProg  $(sdl2-config --cflags --libs) -lSDL2_ttf -lSDL2_mixer -lSDL2_image
+
+if [ $? -eq 0 ]; then
+    echo "Compilation OK"
+    cd bin
+    ./MonProg
+else
+    echo "Erreur de compilation"
+fi
---
+++ LibSDL/tools.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// convertion strtoL strtod
+
+
+
+// Saisie securise
+char *input_read(char *str, int size){
+    char *data;
+
+    data = fgets(str, size, stdin);
+
+    if(data){
+        size_t len = strlen(str) - 1;
+
+        if(str[len] == '\n'){
+            str[len] = '\0';
+        } 
+        else {
+            // vide tempons de lecture
+            fscanf(stdin, "%*[^\n]");
+            fgetc(stdin);
+        }
+    
+    }
+
+    return data;
+}
+
+void flush_input_buffer(void)
+{
+    int c = 0;
+
+    while (c != '\n' && c != EOF){
+        c = getchar();
+    }
+}
+
+int main(void){
+    char buffer[6];
+
+    printf("Mot de 5 lettre : ");
+    input_read(buffer, 6);
+    printf("%s\n", buffer);
+
+    printf("-----------------------------------n");
+
+    printf("Mot de 5 lettre : ");
+    input_read(buffer, 6);
+    printf("%s\n", buffer);
+
+    return 0;
+}

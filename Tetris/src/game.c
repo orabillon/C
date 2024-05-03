bb80e2262ef918c0f5bdc318427129243d1cd48d
orabillon <115501074+orabillon@users.noreply.github.com>
2024-05-03 09:18:18 +0200

    mise a jour

--- Tetris/src/game.c
+++ Tetris/src/game.c
@@ -8,11 +8,21 @@
 #include <math.h>
 
 List* Tetrominos;
+tetros* currentTetros;
+int currentRotation;
+
+tetros* Tetros1;
+tetros* Tetros2;
+tetros* Tetros3;
+tetros* Tetros4;
+tetros* Tetros5;
+tetros* Tetros6;
+tetros* Tetros7;
 
 void funPrint (void* data){
 
     int valeur = ((tetros*)data)->tabTetros[1][2][1];
-    printf(" fonction pointe %d ", valeur);
+    printf(" %d ", valeur);
     
 }
 
@@ -20,12 +30,22 @@ void game_load(void)
 {
     
     Tetrominos = orn_list_newList();
-    
-    tetros TetrosOne;
-
-     
-    int tableau[2][4][4] = {
+    currentRotation = 0;
+         
+    int tableau1[4][4][4] = {
     {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+    },
+      {
         {0, 0, 0, 0},
         {0, 0, 0, 0},
         {1, 1, 1, 1},
@@ -39,13 +59,183 @@ void game_load(void)
     }
     };
 
-    TetrosOne.tabTetros = tableau;
+     int tableau2[4][4][4] = {
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0}
+    }
+    };
+    int tableau3[4][4][4] = {
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 1, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+    },
+     {
+        {1, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    }
+    };
+    int tableau4[4][4][4] = {
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 0},
+        {1, 0, 0, 0}
+    },
+    {
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    },
+     {
+        {0, 0, 1, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+    }
+    };
+    int tableau5[4][4][4] = {
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {1, 1, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    },
+     {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {1, 1, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    }
+    };
+     int tableau6[4][4][4] = {
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 1, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    },
+     {
+        {0, 1, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    }
+    };
+     int tableau7[4][4][4] = {
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+    },
+     {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+    }
+    };
+
+    Tetros1->tabTetros = tableau1;
+    Tetros2->tabTetros = tableau2;
+    Tetros3->tabTetros = tableau3;
+    Tetros4->tabTetros = tableau4;
+    Tetros5->tabTetros = tableau5;
+    Tetros6->tabTetros = tableau6;
+    Tetros7->tabTetros = tableau7;
    
-    Tetrominos = orn_list_addBack(Tetrominos,&TetrosOne);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros1);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros2);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros3);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros4);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros5);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros6);
+    Tetrominos = orn_list_addBack(Tetrominos,&Tetros7);
 
     orn_list_print(Tetrominos," - ", &funPrint);
 
-   orn_list_clear(Tetrominos);
+    currentTetros = Tetros1;
+   
 }
 
 void game_reset(void){
@@ -59,10 +249,10 @@ void game_update(float dt)
 
 void game_draw(void)
 {
-   
+  // int shape[4][4][4]  = tetros1->tabTetros[currentRotation];
 }
 
 void game_close(void)
 {
-    
+    orn_list_clear(Tetrominos);
 }
